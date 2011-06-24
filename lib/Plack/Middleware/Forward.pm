5d4c6e653d0b8b4f5c843bc43ba591c4e77c24e1
Kenta Sato <k-sato@mfac.jp>
2011-06-24 11:23:37 +0900

    initial commit

---
+++ lib/Plack/Middleware/Forward.pm
@@ -0,0 +1,32 @@
+package Plack::Middleware::Forward;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::Forward -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::Forward;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Forward is
+
+=head1 AUTHOR
+
+Kenta Sato E<lt>karupa@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
